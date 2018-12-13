e647874679f9dcd01ab3bbafa5ded02d4eaec165
James Teerayuth <teerayuth.james@gmail.com>
2018-12-13 17:26:25 +0700

    initial commit [Bug] "Expected {} to be a GraphQL schema."

---
+++ schema.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
+} = require("graphql");
+
+const LaunchType = new GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_data_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType }
+  })
+});
+
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
+
+// Query
+const RootQueryType = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.export = new GraphQLSchema({
+  query: RootQueryType
+});
